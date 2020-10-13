80a252b0ed43d22a841ce02c0cb09ec4c68a91ac
Miłosz <miloszstolarczyk@gmail.com>
2020-10-13 20:24:52 +0200

    connect with api

--- src/app/modules/dashboard/dashboard-routing.module.ts
+++ src/app/modules/dashboard/dashboard-routing.module.ts
@@ -1,12 +1,14 @@
 import { DashboardComponent } from './dashboard.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AuthGuard } from '../guards/auth.guard';
 
 
 const routes: Routes = [
   {
     path: "dashboard",
-    component: DashboardComponent
+    component: DashboardComponent,
+    canActivate: [AuthGuard],
   }
 ];
 
